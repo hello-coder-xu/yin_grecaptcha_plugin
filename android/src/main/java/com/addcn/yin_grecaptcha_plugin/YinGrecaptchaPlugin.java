c93b23f3d8651d26eb5a9fe7f8f2031fea921a5b
尹旭 <yin_8023@addcn.com>
2019-04-10 19:20:24 +0800

    google recaptcha v2

--- android/src/main/java/com/addcn/yin_grecaptcha_plugin/YinGrecaptchaPlugin.java
+++ android/src/main/java/com/addcn/yin_grecaptcha_plugin/YinGrecaptchaPlugin.java
@@ -6,20 +6,25 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** YinGrecaptchaPlugin */
+/**
+ * YinGrecaptchaPlugin
+ */
 public class YinGrecaptchaPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "yin_grecaptcha_plugin");
-    channel.setMethodCallHandler(new YinGrecaptchaPlugin());
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(),
+                "yin_grecaptcha_plugin");
+        channel.setMethodCallHandler(new YinGrecaptchaPlugin());
     }
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
